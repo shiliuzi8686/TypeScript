5006e3a4f381741fd17be5bc0332500d02477bdd
shiliu <yuting2.liu@ly.com>
2023-08-17 10:34:50 +0800

    feat: 原始类型

--- pamphlet/src/原始类型.ts
+++ pamphlet/src/原始类型.ts
@@ -1,10 +1,30 @@
-// null、undefined
+// null、undefined  
+// --》这两者在没有开启 strictNullChecks 检查的情况下，会被视作其他类型的子类型
 const nullTest: null = null
 const undeTest: undefined = undefined
+// const tmp3: string = null; // 仅在关闭 strictNullChecks 时成立，下同
+// const tmp4: string = undefined;
+
 
 // string
 const superTypeStr1: string = null
 const superTypeStr2: string = undefined
+
+// number
+const age: number = 24;
+
+// boolean
+const male: boolean = false;
+
+// object
+const obj: object = { name, age, male };
+
+// symbol
+const symbolVar: symbol = Symbol('unique');
+
+// bigint
+// const bigintVar1: bigint = 9007199254740991n;
+// const bigintVar2: bigint = BigInt(9007199254740991);
 
 // void
 function func1() {} //返回值类型被隐式推导为void
@@ -27,3 +47,32 @@ const tupple2: [name: string, age: number] = ['ting', 18]
 const tupple3: [name: string, age?: number] = ['liu']
 // tupple1[7]
 // const [num, str, bool, rest] = tupple1
+
+// 对象
+//  interface 声明一个结构，然后使用这个结构来作为一个对象的类型标注
+// interface IDescription {
+//     name: string;
+//     age: number;
+//     male: boolean;
+// }
+// const obj1: IDescription = {
+//     name: 'linbudu',
+//     age: 599,
+//     male: true,
+// };
+
+// 修饰接口属性
+//  ？ --》 可选属性【相当于undefind类型，是吗？】
+interface IDescription {
+    name: string;
+    age: number;
+    male?: boolean;
+    func?: Function;
+}
+
+const obj2: IDescription = {
+    name: 'linbudu',
+    age: 599,
+    male: true,
+    // 无需实现 func 也是合法的
+};
